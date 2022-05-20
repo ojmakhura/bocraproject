4e8702c61acdb736aa4803c212aaca51a4b8b722
Onalenna J. Makhura <ojmakhura@gmail.com>
2022-05-20 10:00:01 +0200

    started reports

--- bocraportal/angular/src/app/app.module.ts
+++ bocraportal/angular/src/app/app.module.ts
@@ -53,18 +53,18 @@ import { DocumentTypeControllerImpl } from '@app/controller/document/type/docume
 import { DocumentModule } from '@app/view/document/document.module';
 import { DocumentControllerImpl } from '@app/controller/document/document-controller.impl';
 import { UseCaseScope } from '@app/utils/use-case-scope';
-import { FormRestControllerImpl } from '@app/service/bw/org/bocra/portal/form/form-rest-controller.impl';
 import { DocumentRestControllerImpl } from '@app/service/bw/org/bocra/portal/document/document-rest-controller.impl';
-import { LicenceTypeRestControllerImpl } from '@app/service/bw/org/bocra/portal/licence/type/licence-type-rest-controller.impl';
-import { LicenseeRestControllerImpl } from '@app/service/bw/org/bocra/portal/licensee/licensee-rest-controller.impl';
-import { SubmissionRestControllerImpl } from '@app/service/bw/org/bocra/portal/form/submission/submission-rest-controller.impl';
-import { UserRestControllerImpl } from '@app/service/bw/org/bocra/portal/user/user-rest-controller.impl';
 import { FormFieldRestControllerImpl } from '@app/service/bw/org/bocra/portal/form/field/form-field-rest-controller.impl';
-import { DocumentTypeRestControllerImpl } from '@app/service/bw/org/bocra/portal/document/type/document-type-rest-controller.impl';
 import { PeriodConfigRestControllerImpl } from '@app/service/bw/org/bocra/portal/period/config/period-config-rest-controller.impl';
-import { PeriodRestControllerImpl } from '@app/service/bw/org/bocra/portal/period/period-rest-controller.impl';
 import { LicenceRestControllerImpl } from '@app/service/bw/org/bocra/portal/licence/licence-rest-controller.impl';
+import { DocumentTypeRestControllerImpl } from '@app/service/bw/org/bocra/portal/document/type/document-type-rest-controller.impl';
+import { UserRestControllerImpl } from '@app/service/bw/org/bocra/portal/user/user-rest-controller.impl';
 import { AuthorisationRestControllerImpl } from '@app/service/bw/org/bocra/portal/auth/authorisation-rest-controller.impl';
+import { PeriodRestControllerImpl } from '@app/service/bw/org/bocra/portal/period/period-rest-controller.impl';
+import { SubmissionRestControllerImpl } from '@app/service/bw/org/bocra/portal/form/submission/submission-rest-controller.impl';
+import { FormRestControllerImpl } from '@app/service/bw/org/bocra/portal/form/form-rest-controller.impl';
+import { LicenseeRestControllerImpl } from '@app/service/bw/org/bocra/portal/licensee/licensee-rest-controller.impl';
+import { LicenceTypeRestControllerImpl } from '@app/service/bw/org/bocra/portal/licence/type/licence-type-rest-controller.impl';
 import { AuthModule } from './auth';
 true
 
@@ -106,18 +106,18 @@ true
     ],
     providers: [
         UseCaseScope,
-        FormRestControllerImpl,
         DocumentRestControllerImpl,
-        LicenceTypeRestControllerImpl,
-        LicenseeRestControllerImpl,
-        SubmissionRestControllerImpl,
-        UserRestControllerImpl,
         FormFieldRestControllerImpl,
-        DocumentTypeRestControllerImpl,
         PeriodConfigRestControllerImpl,
-        PeriodRestControllerImpl,
         LicenceRestControllerImpl,
+        DocumentTypeRestControllerImpl,
+        UserRestControllerImpl,
         AuthorisationRestControllerImpl,
+        PeriodRestControllerImpl,
+        SubmissionRestControllerImpl,
+        FormRestControllerImpl,
+        LicenseeRestControllerImpl,
+        LicenceTypeRestControllerImpl,
         LicenseeControllerImpl,
         UserControllerImpl,
         AuthorisationControllerImpl,
