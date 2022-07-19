9354ef6ee313c4b5746f8a503e3a38d1184e6e08
Onalenna Junior Makhura <ojmakhura@gmail.com>
2022-07-19 12:42:15 +0200

    building docker containers

--- bocraportal/webservice/src/main/java/bw/org/bocra/portal/auth/AuthorisationRestControllerImpl.java
+++ bocraportal/webservice/src/main/java/bw/org/bocra/portal/auth/AuthorisationRestControllerImpl.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("authorisation")
+@CrossOrigin()
 @Tag(name = "authorisations", description = "Managing access authorisations")
 public class AuthorisationRestControllerImpl extends AuthorisationRestControllerBase {
 
