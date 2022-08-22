5e966efcd51c5e497455e3d2cd32fb16e3ada7f5
Onalenna Junior Makhura <ojmakhura@roguesystems.co.bw>
2022-08-22 09:50:46 +0200

    Status from string to enum for form submission

--- bocraportal/core/src/main/java/bw/org/bocra/portal/licensee/LicenseeDaoImpl.java
+++ bocraportal/core/src/main/java/bw/org/bocra/portal/licensee/LicenseeDaoImpl.java
@@ -13,6 +13,7 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 
+import bw.org.bocra.portal.complaint.ComplaintRepository;
 import bw.org.bocra.portal.document.Document;
 import bw.org.bocra.portal.document.DocumentRepository;
 import bw.org.bocra.portal.document.DocumentVO;
@@ -55,13 +56,13 @@ public class LicenseeDaoImpl
             SectorRepository sectorRepository, ShareholderRepository shareholderRepository,
             LicenseeShareholderRepository licenseeShareholderRepository,
             LicenseeSectorRepository licenseeSectorRepository, LicenseeFormRepository licenseeFormRepository,
-            LicenseeReportConfigRepository licenseeReportConfigRepository,
+            LicenseeReportConfigRepository licenseeReportConfigRepository, ComplaintRepository complaintRepository,
             NotificationRepository notificationRepository, LicenseeRepository licenseeRepository) {
 
         super(licenseeUserRepository, formSubmissionRepository, formRepository, licenceRepository, documentRepository,
                 reportRepository, reportConfigRepository, sectorRepository, shareholderRepository,
                 licenseeShareholderRepository, licenseeSectorRepository, licenseeFormRepository, licenseeReportConfigRepository,
-                notificationRepository, licenseeRepository);
+                notificationRepository, complaintRepository, licenseeRepository);
     }
 
     /**
