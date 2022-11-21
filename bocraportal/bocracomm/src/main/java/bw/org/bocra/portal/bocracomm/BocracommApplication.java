784f2cec3dd3cd22f033bca0d99eb56babe13d95
Onalenna Junior Makhura <ojmakhura@roguesystems.co.bw>
2022-11-21 14:22:12 +0200

    Added comm module

---
+++ bocraportal/bocracomm/src/main/java/bw/org/bocra/portal/bocracomm/BocracommApplication.java
@@ -0,0 +1,13 @@
+package bw.org.bocra.portal.bocracomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BocracommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BocracommApplication.class, args);
+	}
+
+}
---
+++ bocraportal/bocracomm/src/test/java/bw/org/bocra/portal/bocracomm/BocracommApplicationTests.java
@@ -0,0 +1,13 @@
+package bw.org.bocra.portal.bocracomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BocracommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
