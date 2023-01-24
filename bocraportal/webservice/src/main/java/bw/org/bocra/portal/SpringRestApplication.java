45cd703b73de262f2287c396cfea7dd860d0b44b
Onalenna Makhura <ojmakhura@gmail.com>
2023-01-24 04:33:01 +0200

    Properties read

--- bocraportal/webservice/src/main/java/bw/org/bocra/portal/SpringRestApplication.java
+++ bocraportal/webservice/src/main/java/bw/org/bocra/portal/SpringRestApplication.java
@@ -1,21 +1,23 @@
 package bw.org.bocra.portal;
 
-import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Import;
 
-import bw.org.bocra.portal.period.PeriodService;
-import bw.org.bocra.portal.period.config.PeriodConfigService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @SpringBootApplication
 @Import(SharedAutoConfiguration.class)
+// @OpenAPIDefinition(info = @Info(title = "BOCRA Portal API", description = "The main API for the BOCRA portal."))
+// @SecurityScheme(name = "bocraportal-api", type = SecuritySchemeType.HTTP, scheme = "basic", in = SecuritySchemeIn.HEADER)
+@ConfigurationPropertiesScan({"bw.org.bocra.portal.properties"})
 public class SpringRestApplication {
 
 	private Logger log = LoggerFactory.getLogger(getClass());
@@ -23,17 +25,4 @@ public class SpringRestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApplication.class, args);
 	}
-
-	// @Bean
-	// public ApplicationRunner applicationRunner(PeriodService periodService, PeriodConfigService periodConfigService) {
-		
-	// 	return (args) -> {
-
-	// 		if(CollectionUtils.isEmpty(periodService.getAll())) {
-	// 			log.info("Bocraportal system not initialised. Begin initialisation ....");
-	// 			log.info("Initialising period configurations .... ");
-
-	// 		}
-	// 	};
-	// }
 }
